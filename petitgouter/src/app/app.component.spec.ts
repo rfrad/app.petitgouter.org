109c6621f6c3d29d66cd81e8cd248ed89052e9f7
Remi Fradet <remi@drcanary.com>
2021-07-01 11:32:46 +0200

    This is what happens when you do not follow TDD.

--- petitgouter/src/app/app.component.spec.ts
+++ petitgouter/src/app/app.component.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { environment } from 'src/environments/environment';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
@@ -30,6 +31,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('petitgouter app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('Welcome to the environment ' + environment.envName + ' of my-domain SPA!');
   });
 });
