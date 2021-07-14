7ffcc8d43e9126064eb309cc8f2412ce2562498f
Remi Fradet <remi@drcanary.com>
2021-07-14 15:30:39 +0100

    Remove unused configuration

--- petitgouter/karma.conf.js
+++ petitgouter/karma.conf.js
@@ -44,7 +44,6 @@ module.exports = function (config) {
         base: 'ChromeHeadless',
         flags: ['--no-sandbox', '--disable-gpu']
       }
-    },
-    restartOnFileChange: true
+    }
   });
 };
