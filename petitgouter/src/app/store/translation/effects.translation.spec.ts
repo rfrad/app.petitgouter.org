3cba55f180bb2ceae0e2c61b97d69fb024d89bbd
Remi Fradet <remi@drcanary.com>
2022-08-20 00:55:14 +0100

    Load translations from files

--- petitgouter/src/app/store/translation/effects.translation.spec.ts
+++ petitgouter/src/app/store/translation/effects.translation.spec.ts
@@ -1,7 +1,8 @@
+import { HttpClient } from "@angular/common/http";
 import { TestBed } from "@angular/core/testing";
 import { Actions } from "@ngrx/effects";
 import { Action } from "@ngrx/store";
-import { BehaviorSubject } from "rxjs";
+import { BehaviorSubject, of } from "rxjs";
 import { LanguageCode } from "src/app/model/translation.model";
 import { LoadTranslations, TranslationAction } from "./actions.translation";
 import { TranslationEffects } from "./effects.translation";
@@ -9,13 +10,23 @@ import { TranslationEffects } from "./effects.translation";
 describe('TranslationEffects', () => {
     let effects: TranslationEffects;
     let actionPublisher: BehaviorSubject<Action>;
+
+    const fileLoaderMock = {
+        get: (file: string) => of({
+            name: 'HARRY'
+        })
+    }
   
     beforeEach(() => {
         actionPublisher = new BehaviorSubject(<Action>{ type: 'initForTest' });
+        spyOn(fileLoaderMock, 'get').and.callThrough()
         TestBed.configureTestingModule({
             providers: [{
                 provide: Actions,
                 useValue: actionPublisher
+            }, {
+                provide: HttpClient,
+                useValue: fileLoaderMock
             }]
         });
         effects = TestBed.inject(TranslationEffects);
@@ -33,6 +44,7 @@ describe('TranslationEffects', () => {
         const sub = effects.loadTranslations$.subscribe(action => {
             // Then is should load a dummy translation set (for now)
             expect(action.type).toEqual(TranslationAction.LOAD_TRANSLATIONS_SUCCESS);
+            expect(fileLoaderMock.get).toHaveBeenCalledWith(`assets/config/translation/${LanguageCode.en}.json`)
             expect(action.translations).toEqual({ name: 'HARRY'});
 
             sub.unsubscribe();
--- petitgouter/src/app/store/translation/effects.translation.ts
+++ petitgouter/src/app/store/translation/effects.translation.ts
@@ -2,24 +2,29 @@ import { Injectable } from "@angular/core";
 import { Actions, createEffect, ofType } from "@ngrx/effects";
 import { of, switchMap } from "rxjs";
 import { LoadTranslations, LoadTranslationsSuccess } from "./actions.translation";
+import { HttpClient } from '@angular/common/http'
+import { Translations } from "src/app/model/translation.model";
 
 @Injectable({ providedIn: 'root' })
 export class TranslationEffects {
 
     constructor(
-        private actions$: Actions
+        private actions$: Actions,
+        private httpClient: HttpClient
     ) {}
 
     loadTranslations$ = createEffect(() => 
         this.actions$.pipe(
             ofType(LoadTranslations),
-            switchMap(action => {
-                return of(LoadTranslationsSuccess({
-                    translations: {
-                        name: 'HARRY'
-                    }
-                }));
-            })
+            switchMap(action => 
+                this.httpClient.get(`assets/config/translation/${action.code}.json`).pipe(
+                    switchMap(translations => 
+                        of(LoadTranslationsSuccess({ 
+                            translations: <Translations>translations 
+                        }))
+                    )
+                )
+            )
         )
     );
 }
