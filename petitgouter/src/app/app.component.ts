f3e856827211c96d8edc4956d80b437b415f0394
Remi Fradet <remi@drcanary.com>
2022-08-23 23:20:22 +0100

    Add overlayers on main page

--- petitgouter/src/app/app.component.ts
+++ petitgouter/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { environment } from 'src/environments/environment';
 import { LanguageCode } from './model/translation.model';
+import { OverLayerService } from './services/utils/over-layer.service';
 import { AppState } from './store/store.state';
 import { LoadTranslations } from './store/translation/translation.actions';
 
@@ -14,7 +15,8 @@ export class AppComponent implements OnInit {
   appVersion = environment.appVersion;
 
   constructor(
-    private store: Store<AppState>
+    private store: Store<AppState>,
+    readonly overlayerService: OverLayerService
   ){}
 
   ngOnInit(): void {
