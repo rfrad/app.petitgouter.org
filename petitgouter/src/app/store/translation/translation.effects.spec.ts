1be63504f824fdb54764af3d5869258e3f39265d
Remi Fradet <remi@drcanary.com>
2022-08-28 22:09:29 +0100

    Makes sure loadTranslations$ works with new service

--- petitgouter/src/app/store/translation/translation.effects.spec.ts
+++ petitgouter/src/app/store/translation/translation.effects.spec.ts
@@ -1,7 +1,7 @@
-import { HttpClient } from "@angular/common/http";
 import { TestBed } from "@angular/core/testing";
 import { Actions } from "@ngrx/effects";
 import { Action } from "@ngrx/store";
+import { TranslateService } from "@ngx-translate/core";
 import { BehaviorSubject, of } from "rxjs";
 import { LanguageCode } from "src/app/model/translation.model";
 import { LoadTranslations, TranslationAction } from "./translation.actions";
@@ -16,17 +16,22 @@ describe('TranslationEffects', () => {
             name: 'HARRY'
         })
     }
-  
+
+    const translateService = {
+        use: (code: LanguageCode) => {}
+    }
+
     beforeEach(() => {
         actionPublisher = new BehaviorSubject(<Action>{ type: 'initForTest' });
+        spyOn(translateService, 'use').and.stub();
         spyOn(fileLoaderMock, 'get').and.callThrough()
         TestBed.configureTestingModule({
             providers: [{
                 provide: Actions,
                 useValue: actionPublisher
             }, {
-                provide: HttpClient,
-                useValue: fileLoaderMock
+                provide: TranslateService,
+                useValue: translateService
             }]
         });
         effects = TestBed.inject(TranslationEffects);
@@ -36,20 +41,24 @@ describe('TranslationEffects', () => {
         expect(effects).toBeTruthy();
     });
 
-    it('should load the default translations', done => {
-        // Given a LoadTranslation action 
-        const action = LoadTranslations({ code: LanguageCode.en });
+    describe('loadTranslations$', () => {
+        [ LanguageCode.en, LanguageCode.fr ].forEach(language => {
+            it('should set the default translations', done => {
+                // Given a LoadTranslation action 
+                const action = LoadTranslations({ code: language });
 
-        // When the action is dispatched
-        const sub = effects.loadTranslations$.subscribe(action => {
-            // Then is should load a dummy translation set (for now)
-            expect(action.type).toEqual(TranslationAction.LOAD_TRANSLATIONS_SUCCESS);
-            expect(fileLoaderMock.get).toHaveBeenCalledWith(`assets/config/translation/${LanguageCode.en}.json`)
-            expect(action.translations).toEqual({ name: 'HARRY'});
+                // When the action is dispatched
+                const sub = effects.loadTranslations$.subscribe(action => {
+                    // Then is should use the specific language
+                    expect(translateService.use).toHaveBeenCalledOnceWith(language);
+                    expect(action.type).toEqual(TranslationAction.LOAD_TRANSLATIONS_SUCCESS);
+                    expect(action.translations).toEqual({});
 
-            sub.unsubscribe();
-            done();
+                    sub.unsubscribe();
+                    done();
+                });
+                actionPublisher.next(action);
+            });
         });
-        actionPublisher.next(action);
     });
 });
--- petitgouter/src/app/store/translation/translation.effects.ts
+++ petitgouter/src/app/store/translation/translation.effects.ts
@@ -2,29 +2,25 @@ import { Injectable } from "@angular/core";
 import { Actions, createEffect, ofType } from "@ngrx/effects";
 import { of, switchMap } from "rxjs";
 import { LoadTranslations, LoadTranslationsSuccess } from "./translation.actions";
-import { HttpClient } from '@angular/common/http'
-import { Translations } from "src/app/model/translation.model";
+import { TranslateService } from "@ngx-translate/core";
 
 @Injectable({ providedIn: 'root' })
 export class TranslationEffects {
 
     constructor(
         private actions$: Actions,
-        private httpClient: HttpClient
+        private translate: TranslateService
     ) {}
 
     loadTranslations$ = createEffect(() => 
         this.actions$.pipe(
             ofType(LoadTranslations),
-            switchMap(action => 
-                this.httpClient.get(`assets/config/translation/${action.code}.json`).pipe(
-                    switchMap(translations => 
-                        of(LoadTranslationsSuccess({ 
-                            translations: <Translations>translations 
-                        }))
-                    )
-                )
-            )
+            switchMap(action => {
+                this.translate.use(action.code);
+                return of(LoadTranslationsSuccess({ 
+                    translations: {} 
+                }));
+            })
         )
     );
 }
