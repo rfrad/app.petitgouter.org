8a34605a89b0486ec054395b3d1d5fbefe085841
Remi Fradet <remi@drcanary.com>
2022-09-20 23:35:32 +0100

    Create English privacy policy

---
+++ petitgouter/src/app/modules/legal/legal-english/legal-english.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
+
+
+
+@NgModule({
+  declarations: [
+    PrivacyPolicyComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    PrivacyPolicyComponent
+  ]
+})
+export class LegalEnglishModule { }
