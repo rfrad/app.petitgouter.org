c74492bd6e98e30f558fa342a2f41162da4f8b63
rfrad <remi.fradet@gmail.com>
2023-09-02 23:17:36 +0100

    Define zise of icon base on text size
    No unit tests s I do not know how to test getComputedStyle

--- application/src/app/components/utils/svg-icon/svg-icon.component.ts
+++ application/src/app/components/utils/svg-icon/svg-icon.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, Renderer2 } from '@angular/core';
 import { SvgIcon } from 'src/app/model/utils/svg-icon.model';
 import { SvgIconService } from 'src/app/services/utils/svg-icon.service';
 
@@ -11,13 +11,20 @@ export class SvgIconComponent implements OnInit {
   
   @Input() icon!: SvgIcon;
   location: string;
+  size: string;
 
   constructor(
-    private svgLoader: SvgIconService
+    private svgLoader: SvgIconService,
+    private host: ElementRef,
+    private renderer: Renderer2
   ) { }
 
   ngOnInit(): void {
     this.location = this.svgLoader.getSvgFileFor(this.icon);
+    const style: any = getComputedStyle(this.host.nativeElement);
+    this.size = style['font-size'];
+    this.renderer.setStyle(this.host.nativeElement, 'height', this.size); 
+    this.renderer.setStyle(this.host.nativeElement, 'width', this.size); 
   }
 
 }
