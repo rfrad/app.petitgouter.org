ea95cfa7981e9c0a9edfa093fcbcdf0a5a3ef94f
rfrad <remi.fradet@gmail.com>
2023-10-21 00:08:48 +0100

    Create flex-direction directive

---
+++ application/src/app/directives/local-flex-direction.directive.spec.ts
@@ -0,0 +1,8 @@
+import { LocalFlexDirectionDirective } from './local-flex-direction.directive';
+
+describe('LocalFlexDirectionDirective', () => {
+  it('should create an instance', () => {
+    const directive = new LocalFlexDirectionDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ application/src/app/directives/local-flex-direction.directive.ts
@@ -0,0 +1,10 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appLocalFlexDirection]'
+})
+export class LocalFlexDirectionDirective {
+
+  constructor() { }
+
+}
