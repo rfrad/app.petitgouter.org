159e9acb0cd79158afc5672181de3a1bf548d20e
rfrad <remi.fradet@gmail.com>
2023-10-22 00:53:51 +0100

    Dulicate localFlexDirection into localTextDirection

--- application/src/app/directives/local-text-direction/local-text-direction.directive.spec.ts
+++ application/src/app/directives/local-text-direction/local-text-direction.directive.spec.ts
@@ -1,8 +1,68 @@
+import { Component } from '@angular/core';
 import { LocalTextDirectionDirective } from './local-text-direction.directive';
+import { BehaviorSubject } from 'rxjs';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TranslateService } from '@ngx-translate/core';
 
-describe('LocalTextDirectionDirective', () => {
-  it('should create an instance', () => {
-    const directive = new LocalTextDirectionDirective();
-    expect(directive).toBeTruthy();
+describe('LocalTextDirectionDirective', () => {  
+  @Component({
+    template: '<div pgLocalTextDirection></div>'
+  })
+  class Wrapper {}
+
+  const translateServiceMock = {
+    onLangChange: new BehaviorSubject({ 
+      translations: { "language:direction": "flat" }
+    })
+  };
+
+  let fixture: ComponentFixture<Wrapper>;
+
+  beforeEach(async () => {
+    translateServiceMock.onLangChange.next({ 
+      translations: { "language:direction": "vertical" }
+    });
+    await TestBed.configureTestingModule({
+      providers: [
+        { provide: TranslateService, useValue: translateServiceMock },
+      ],
+      declarations: [
+        Wrapper,
+        LocalTextDirectionDirective
+      ]
+    }).compileComponents();
+    fixture = TestBed.createComponent(Wrapper);
+    fixture.detectChanges();
+  });
+
+  it('should have the default class when initilised', () => {
+    expect(fixture.nativeElement.querySelector('div').classList).toContain('text-vertical');
+  });
+
+  it('should have updated the class when language direction has changed', () => {
+    translateServiceMock.onLangChange.next({
+      translations: { "language:direction": "horizontal" }
+    });
+    fixture.detectChanges();
+    expect(fixture.nativeElement.querySelector('div').classList).not.toContain('text-vertical');
+    expect(fixture.nativeElement.querySelector('div').classList).toContain('text-horizontal');
+  });
+
+  it('should use the default left-to-right when no language:direction is provided', () => {
+    translateServiceMock.onLangChange.next({
+      translations: <any>{}
+    });
+    fixture.detectChanges();
+    expect(fixture.nativeElement.querySelector('div').classList).not.toContain('text-vertical');
+    expect(fixture.nativeElement.querySelector('div').classList).not.toContain('text-horizontal');
+    expect(fixture.nativeElement.querySelector('div').classList).toContain('text-left-to-right');
+  });
+
+  it('should use the default left-to-right when no translations is provided', () => {
+    translateServiceMock.onLangChange.next(<any>{});
+    fixture.detectChanges();
+    expect(fixture.nativeElement.querySelector('div').classList).not.toContain('text-vertical');
+    expect(fixture.nativeElement.querySelector('div').classList).not.toContain('text-horizontal');
+    expect(fixture.nativeElement.querySelector('div').classList).toContain('text-left-to-right');
   });
 });
--- application/src/app/directives/local-text-direction/local-text-direction.directive.ts
+++ application/src/app/directives/local-text-direction/local-text-direction.directive.ts
@@ -1,10 +1,30 @@
-import { Directive } from '@angular/core';
+import { Directive, HostBinding, OnDestroy, OnInit } from '@angular/core';
+import { LangChangeEvent, TranslateService } from '@ngx-translate/core';
+import { Subscription } from 'rxjs';
+
+const DEFAULT_LANGUAGE_DIRECTION_CLASS: string = 'left-to-right';
 
 @Directive({
   selector: '[pgLocalTextDirection]'
 })
-export class LocalTextDirectionDirective {
+export class LocalTextDirectionDirective implements OnInit, OnDestroy {
 
-  constructor() { }
+  private sub: Subscription;
+  
+  @HostBinding('class') flexClass = DEFAULT_LANGUAGE_DIRECTION_CLASS;
+
+  constructor(
+    private translations: TranslateService
+  ) {}
+
+  ngOnInit(): void {
+    this.sub = this.translations.onLangChange.subscribe((event: LangChangeEvent) => {
+      this.flexClass = `text-${event.translations?.['language:direction'] || DEFAULT_LANGUAGE_DIRECTION_CLASS}`;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
 
 }
