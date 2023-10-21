3957a81c3545b970a77fd7cff970389a06d291e4
rfrad <remi.fradet@gmail.com>
2023-10-21 01:31:16 +0100

    Create directive

--- application/src/app/directives/local-flex-direction.directive.spec.ts
+++ application/src/app/directives/local-flex-direction.directive.spec.ts
@@ -1,8 +1,47 @@
+import { Component } from '@angular/core';
 import { LocalFlexDirectionDirective } from './local-flex-direction.directive';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { BehaviorSubject } from 'rxjs';
+import { TranslateService } from '@ngx-translate/core';
 
 describe('LocalFlexDirectionDirective', () => {
-  it('should create an instance', () => {
-    const directive = new LocalFlexDirectionDirective();
-    expect(directive).toBeTruthy();
+  @Component({
+    template: '<div appLocalFlexDirection></div>'
+  })
+  class Wrapper {}
+
+  const translateServiceMock = {
+    onLangChange: new BehaviorSubject({ 
+      translations: { "language:direction": "vertical" }
+    })
+  };
+
+  let fixture: ComponentFixture<Wrapper>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      providers: [
+        { provide: TranslateService, useValue: translateServiceMock },
+      ],
+      declarations: [
+        Wrapper,
+        LocalFlexDirectionDirective
+      ]
+    }).compileComponents();
+    fixture = TestBed.createComponent(Wrapper);
+    fixture.detectChanges();
+  });
+
+  it('should have the default class when initilised', () => {
+    expect(fixture.nativeElement.querySelector('div').classList).toContain('vertical');
+  });
+
+  it('should have updated the class when language direction has changed', () => {
+    translateServiceMock.onLangChange.next({
+      translations: { "language:direction": "horizontal" }
+    });
+    fixture.detectChanges();
+    expect(fixture.nativeElement.querySelector('div').classList).not.toContain('vertical');
+    expect(fixture.nativeElement.querySelector('div').classList).toContain('horizontal');
   });
 });
--- application/src/app/directives/local-flex-direction.directive.ts
+++ application/src/app/directives/local-flex-direction.directive.ts
@@ -1,10 +1,28 @@
-import { Directive } from '@angular/core';
+import { Directive, HostBinding, OnDestroy, OnInit } from '@angular/core';
+import { LangChangeEvent, TranslateService } from '@ngx-translate/core';
+import { Subscription } from 'rxjs';
 
 @Directive({
   selector: '[appLocalFlexDirection]'
 })
-export class LocalFlexDirectionDirective {
+export class LocalFlexDirectionDirective implements OnInit, OnDestroy {
 
-  constructor() { }
+  private sub: Subscription;
+  
+  @HostBinding('class') flexClass = 'left-to-right';
+
+  constructor(
+    private translations: TranslateService
+  ) {}
+
+  ngOnInit(): void {
+    this.sub = this.translations.onLangChange.subscribe((event: LangChangeEvent) => {
+      this.flexClass = event.translations['language:direction'];
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
 
 }
