d89ab411bca5c5f326fdfd2f800377a4590b4cde
rfrad <remi.fradet@gmail.com>
2025-02-21 18:39:48 +0000

    Cover directive with unit tests

--- application/src/app/directives/local-text-direction/local-text-direction.directive.spec.ts
+++ application/src/app/directives/local-text-direction/local-text-direction.directive.spec.ts
@@ -4,7 +4,7 @@ import { BehaviorSubject } from 'rxjs';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { TranslateService } from '@ngx-translate/core';
 
-describe('LocalTextDirectionDirective', () => {  
+describe('LocalTextDirectionDirective on component without any classes', () => {  
   @Component({
     template: '<div pgLocalTextDirection></div>'
   })
@@ -20,7 +20,7 @@ describe('LocalTextDirectionDirective', () => {
 
   beforeEach(async () => {
     translateServiceMock.onLangChange.next({ 
-      translations: { "language:direction": "vertical" }
+      translations: { "language:direction": "VERTICAL" }
     });
     await TestBed.configureTestingModule({
       providers: [
@@ -36,16 +36,16 @@ describe('LocalTextDirectionDirective', () => {
   });
 
   it('should have the default class when initilised', () => {
-    expect(fixture.nativeElement.querySelector('div').classList).toContain('text-vertical');
+    expect(fixture.nativeElement.querySelector('div').classList).toContain('text-VERTICAL');
   });
 
   it('should have updated the class when language direction has changed', () => {
     translateServiceMock.onLangChange.next({
-      translations: { "language:direction": "horizontal" }
+      translations: { "language:direction": "DIAGONAL" }
     });
     fixture.detectChanges();
-    expect(fixture.nativeElement.querySelector('div').classList).not.toContain('text-vertical');
-    expect(fixture.nativeElement.querySelector('div').classList).toContain('text-horizontal');
+    expect(fixture.nativeElement.querySelector('div').classList).not.toContain('text-VERTICAL');
+    expect(fixture.nativeElement.querySelector('div').classList).toContain('text-DIAGONAL');
   });
 
   it('should use the default left-to-right when no language:direction is provided', () => {
@@ -53,16 +53,87 @@ describe('LocalTextDirectionDirective', () => {
       translations: <any>{}
     });
     fixture.detectChanges();
-    expect(fixture.nativeElement.querySelector('div').classList).not.toContain('text-vertical');
-    expect(fixture.nativeElement.querySelector('div').classList).not.toContain('text-horizontal');
+    expect(fixture.nativeElement.querySelector('div').classList).not.toContain('text-VERTICAL');
+    expect(fixture.nativeElement.querySelector('div').classList).not.toContain('text-DIAGONAL');
     expect(fixture.nativeElement.querySelector('div').classList).toContain('text-left-to-right');
   });
 
   it('should use the default left-to-right when no translations is provided', () => {
     translateServiceMock.onLangChange.next(<any>{});
     fixture.detectChanges();
-    expect(fixture.nativeElement.querySelector('div').classList).not.toContain('text-vertical');
-    expect(fixture.nativeElement.querySelector('div').classList).not.toContain('text-horizontal');
+    expect(fixture.nativeElement.querySelector('div').classList).not.toContain('text-VERTICAL');
+    expect(fixture.nativeElement.querySelector('div').classList).not.toContain('text-DIAGONAL');
+    expect(fixture.nativeElement.querySelector('div').classList).toContain('text-left-to-right');
+  });
+});
+
+describe('LocalTextDirectionDirective on component with other classes', () => {  
+  @Component({
+    template: '<div pgLocalTextDirection class="klass1 klass2"></div>'
+  })
+  class Wrapper2 {}
+
+  const translateServiceMock = {
+    onLangChange: new BehaviorSubject({ 
+      translations: { "language:direction": "flat" }
+    })
+  };
+
+  let fixture: ComponentFixture<Wrapper2>;
+
+  beforeEach(async () => {
+    translateServiceMock.onLangChange.next({ 
+      translations: { "language:direction": "IN_3D" }
+    });
+    await TestBed.configureTestingModule({
+      providers: [
+        { provide: TranslateService, useValue: translateServiceMock },
+      ],
+      declarations: [
+        Wrapper2,
+        LocalTextDirectionDirective
+      ]
+    }).compileComponents();
+    fixture = TestBed.createComponent(Wrapper2);
+    fixture.detectChanges();
+  });
+
+  it('should have the default class when initilised', () => {
+    expect(fixture.nativeElement.querySelector('div').classList).toContain('klass1');
+    expect(fixture.nativeElement.querySelector('div').classList).toContain('klass2');
+    expect(fixture.nativeElement.querySelector('div').classList).toContain('text-IN_3D');
+  });
+
+  it('should have updated the class when language direction has changed', () => {
+    translateServiceMock.onLangChange.next({
+      translations: { "language:direction": "HORIZONTAL" }
+    });
+    fixture.detectChanges();
+    expect(fixture.nativeElement.querySelector('div').classList).toContain('klass1');
+    expect(fixture.nativeElement.querySelector('div').classList).toContain('klass2');
+    expect(fixture.nativeElement.querySelector('div').classList).not.toContain('text-IN_3D');
+    expect(fixture.nativeElement.querySelector('div').classList).toContain('text-HORIZONTAL');
+  });
+
+  it('should use the default left-to-right when no language:direction is provided', () => {
+    translateServiceMock.onLangChange.next({
+      translations: <any>{}
+    });
+    fixture.detectChanges();
+    expect(fixture.nativeElement.querySelector('div').classList).not.toContain('text-IN_3D');
+    expect(fixture.nativeElement.querySelector('div').classList).not.toContain('text-HORIZONTAL');
+    expect(fixture.nativeElement.querySelector('div').classList).toContain('klass1');
+    expect(fixture.nativeElement.querySelector('div').classList).toContain('klass2');
+    expect(fixture.nativeElement.querySelector('div').classList).toContain('text-left-to-right');
+  });
+
+  it('should use the default left-to-right when no translations is provided', () => {
+    translateServiceMock.onLangChange.next(<any>{});
+    fixture.detectChanges();
+    expect(fixture.nativeElement.querySelector('div').classList).not.toContain('text-IN_3D');
+    expect(fixture.nativeElement.querySelector('div').classList).not.toContain('text-HORIZONTAL');
+    expect(fixture.nativeElement.querySelector('div').classList).toContain('klass1');
+    expect(fixture.nativeElement.querySelector('div').classList).toContain('klass2');
     expect(fixture.nativeElement.querySelector('div').classList).toContain('text-left-to-right');
   });
 });
