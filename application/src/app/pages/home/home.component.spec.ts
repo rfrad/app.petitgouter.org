6013fb731e4145104e6f46962c9ad52e2f7a0285
rfrad <remi.fradet@gmail.com>
2025-02-21 19:35:38 +0000

    Add content to home page

--- application/src/app/pages/home/home.component.spec.ts
+++ application/src/app/pages/home/home.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
+import { MockTranslatePipe } from 'src/app/pipes/mock.pipe.spec';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -8,7 +9,10 @@ describe('HomeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      declarations: [ 
+        HomeComponent,
+        MockTranslatePipe
+      ]
     })
     .compileComponents();
 
