8390a4fa9cbeac53d252ec9de59cee3d191a8b0a
rfrad <remi.fradet@gmail.com>
2025-04-05 14:32:11 +0100

    Store language in localStorage when preference fllag is true

--- application/src/app/store/translation/translation.effects.spec.ts
+++ application/src/app/store/translation/translation.effects.spec.ts
@@ -6,6 +6,8 @@ import { BehaviorSubject, of } from "rxjs";
 import { LanguageCode } from "src/app/model/translation.model";
 import { LoadTranslations, TranslationAction } from "./translation.actions";
 import { TranslationEffects } from "./translation.effects";
+import { MockStore, provideMockStore } from "@ngrx/store/testing";
+import { Preference } from "src/app/model/preferences.model";
 
 describe('TranslationEffects', () => {
     let effects: TranslationEffects;
@@ -32,7 +34,17 @@ describe('TranslationEffects', () => {
             }, {
                 provide: TranslateService,
                 useValue: translateService
-            }]
+            },
+            provideMockStore({
+                initialState: {
+                    preferences: {
+                        preferences: {
+                            [Preference.preferences]: true
+                        }
+                    }
+                }
+            })
+        ]
         });
         effects = TestBed.inject(TranslationEffects);
     });
@@ -43,22 +55,79 @@ describe('TranslationEffects', () => {
 
     describe('loadTranslations$', () => {
         [ LanguageCode.en, LanguageCode.fr ].forEach(language => {
-            it('should set the default translations', done => {
-                // Given a LoadTranslation action 
-                const action = LoadTranslations({ code: language });
+            [ true, false ].forEach(storePreferences => {
+                it(`should set the default translations to ${language} when storing preferences is set to ${storePreferences}`, done => {
+                    // Given a LoadTranslation action 
+                    const action = LoadTranslations({ code: language });
+                    const mockStore = TestBed.inject(MockStore);
+                    mockStore.setState({
+                        preferences: {
+                            preferences: {
+                                [Preference.preferences]: storePreferences
+                            }
+                        }
+                    });
 
-                // When the action is dispatched
-                const sub = effects.loadTranslations$.subscribe(action => {
-                    // Then is should use the specific language
-                    expect(translateService.use).toHaveBeenCalledOnceWith(language);
-                    expect(action.type).toEqual(TranslationAction.LOAD_TRANSLATIONS_SUCCESS);
-                    expect(action.translations).toEqual({});
+                    // When the action is dispatched
+                    const sub = effects.loadTranslations$.subscribe(action => {
+                        // Then is should use the specific language
+                        expect(translateService.use).toHaveBeenCalledOnceWith(language);
+                        expect(action.type).toEqual(TranslationAction.LOAD_TRANSLATIONS_SUCCESS);
+                        expect(action.translations).toEqual({});
 
-                    sub.unsubscribe();
-                    done();
+                        sub.unsubscribe();
+                        done();
+                    });
+                    actionPublisher.next(action);
                 });
-                actionPublisher.next(action);
             });
+        });
+
+        it('should save language when preferences flag is true', done => {
+            // Given the preferences flag is true
+            const mockStore = TestBed.inject(MockStore);
+            mockStore.setState({
+                preferences: {
+                    preferences: {
+                        [Preference.preferences]: true
+                    }
+                }
+            });
+            spyOn(localStorage, 'setItem');
+
+            // When loading a new language
+            const action = LoadTranslations({ code: LanguageCode.fr });
+            const sub = effects.loadTranslations$.subscribe(() => {
+                // Then is should store the language in the local storage
+                expect(localStorage.setItem).toHaveBeenCalledOnceWith('translations.languageCode', LanguageCode.fr);
+                sub.unsubscribe();
+                done();
+            });
+            actionPublisher.next(action);
+        });
+        
+
+        it('should NOT save language when preferences flag is false', done => {
+            // Given the preferences flag is false
+            const mockStore = TestBed.inject(MockStore);
+            mockStore.setState({
+                preferences: {
+                    preferences: {
+                        [Preference.preferences]: false
+                    }
+                }
+            });
+            spyOn(localStorage, 'setItem');
+
+            // When loading a new language
+            const action = LoadTranslations({ code: LanguageCode.fr });
+            const sub = effects.loadTranslations$.subscribe(() => {
+                // Then is should NOT store the language in the local storage
+                expect(localStorage.setItem).not.toHaveBeenCalledOnceWith('translations.languageCode', LanguageCode.fr);
+                sub.unsubscribe();
+                done();
+            });
+            actionPublisher.next(action);
         });
     });
 });
--- application/src/app/store/translation/translation.effects.ts
+++ application/src/app/store/translation/translation.effects.ts
@@ -1,21 +1,34 @@
 import { Injectable } from "@angular/core";
 import { Actions, createEffect, ofType } from "@ngrx/effects";
-import { of, switchMap } from "rxjs";
+import { of, switchMap, tap, withLatestFrom } from "rxjs";
 import { LoadTranslations, LoadTranslationsSuccess } from "./translation.actions";
 import { TranslateService } from "@ngx-translate/core";
+import { Store } from "@ngrx/store";
+import { getPreference } from "../preferences/preferences.selectors";
+import { Preference } from "../../model/preferences.model";
+import { AppState } from "../store.state";
 
 @Injectable({ providedIn: 'root' })
 export class TranslationEffects {
 
     constructor(
         private actions$: Actions,
-        private translate: TranslateService
+        private translate: TranslateService,
+        private store: Store<AppState>,
     ) {}
 
     loadTranslations$ = createEffect(() => 
         this.actions$.pipe(
             ofType(LoadTranslations),
-            switchMap(action => {
+            withLatestFrom(
+                this.store.select(getPreference(Preference.preferences))
+            ),
+            tap(([action, savePreferences]) => {
+                if(savePreferences) {
+                    localStorage.setItem('translations.languageCode', action.code);
+                }
+            }),
+            switchMap(([action, _]) => {
                 this.translate.use(action.code);
                 return of(LoadTranslationsSuccess({ 
                     translations: {} 
