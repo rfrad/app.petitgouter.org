38fda2e856309bf89d1d9cd735838c968eccedd6
rfrad <remi.fradet@gmail.com>
2025-04-06 22:09:19 +0100

    Provide a generic type when no props is required to create an action

--- application/src/app/store/common/common.actions.ts
+++ application/src/app/store/common/common.actions.ts
@@ -1,9 +1,11 @@
-import { createAction } from "@ngrx/store";
+import { createAction, props } from "@ngrx/store";
 
 export enum CommonAction {
     INIT_APPLICATION = "[Common] Init",
 }
 
+export type NoProps = any;
 export const InitaliseApplication = createAction(
-    CommonAction.INIT_APPLICATION
+    CommonAction.INIT_APPLICATION,
+    props<NoProps>()
 )
