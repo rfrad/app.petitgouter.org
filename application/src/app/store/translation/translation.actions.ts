2203383a28f445b55b7f1297bc5603f02a3c55f5
rfrad <remi.fradet@gmail.com>
2025-04-06 23:45:26 +0100

    Adding fogotten action

--- application/src/app/store/translation/translation.actions.ts
+++ application/src/app/store/translation/translation.actions.ts
@@ -1,15 +1,21 @@
 import { createAction, props } from "@ngrx/store";
 import { AppError } from "src/app/model/error.model";
 import { LanguageCode, Translations } from "src/app/model/translation.model";
+import { NoProps } from "../common/common.actions";
 
 export enum TranslationAction {
     LOAD_TRANSLATIONS = "[Translation] Load",
     LOAD_TRANSLATIONS_SUCCESS = "[Translation] Load success",
-    LOAD_TRANSLATIONS_ERROR = "[Translation] Load error",
+    SET_DEFAULT_LANGUAGE_SUCCESS = "[Translation] Set default language success",
 }
 
 export type LoadTranslationsProps = { code: LanguageCode };
 export const LoadTranslations = createAction(
     TranslationAction.LOAD_TRANSLATIONS,
     props<LoadTranslationsProps>()
+);
+
+export const LoadTranslationsSuccess = createAction(
+    TranslationAction.LOAD_TRANSLATIONS_SUCCESS,
+    props<NoProps>()
 );
